87cf2541deef117183f7edacfb6b7a9c95e411d2
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-12-24 16:27:32 +0000

    day 5 done

--- day5/Day5.py
+++ day5/Day5.py
@@ -1,43 +1,18 @@
 import hashlib
-code = b"abbhdwsy"
-
 i = 0
-md = hashlib.md5()
-md.update(b"abc"+bytes(i))
-hash = md.hexdigest()
+answer = ""
 
-
-while not hash.startswith("00000"):
-    i += 1
-    md.update(b"abc"+bytes(i))
+while len(answer) < 8:
+    md = hashlib.md5()
+    # add integer to door id and encode as bytes (casting to bytes breaks for some reason)
+    code = ('abbhdwsy'+str(i)).encode('utf-8')
+    md.update(code)
     hash = md.hexdigest()
-    print(i, hash)
 
+    # check if hex digest starts with 5 zeros and add to answer if it does
+    if hash.startswith("00000"):
+        answer += str(hash[5])
 
+    i += 1
 
-md.update(b"abc"+b"3231929")
-print(md.hexdigest())
-print(md.hexdigest().startswith("00000"))
-
-import hashlib
-import random
-index = 0
-password = '________'
-while 1:
-    m = hashlib.md5()
-    m.update(('abc'+str(index)).encode('utf-8'))
-    hex_m = m.hexdigest()
-    if hex_m[0:5] == '00000':
-        password_pos = int(hex_m[5], 16)
-        if password_pos < 8:
-            password_dig = int(hex_m[6], 16)
-            if password[password_pos] == '_':
-                password = password[:password_pos] + hex(password_dig)[-1] + password[password_pos + 1:]
-    if index % 30000 == 0:
-        for char in password:
-            if char == '_':
-                print(str(random.random())[-1], end='')
-            else:
-                print(char, end='')
-        print('\r', end='')
-    index += 1
+print(answer)
