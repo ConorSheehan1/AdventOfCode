03db91192e288611c2ecb964dae17d61d634ba2a
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-12-24 23:45:35 +0000

    checkpoint before tyring with file

--- day8/Day8.py
+++ day8/Day8.py
@@ -1,16 +1,34 @@
-def shift_row(row, length):
+def shift(row, length):
     return row[-length:] + row[:-length]
+
+
+def shift_row(matrix, row, length):
+    matrix[row] = shift(matrix[row], length)
+
 
 # shift column in place
 def shift_col(matrix, col_index, length):
     l = []
     for col in matrix:
         l.append(col[col_index])
-    l = shift_row(l, length)
+    l = shift(l, length)
     for i in range(len(l)):
         matrix[i][col_index] = l[i]
 
 
+def rect(matrix, x, y):
+    # rectangle should b x wide and y tall
+    for i in range(x):
+        for j in range(y):
+            matrix[j][i] = "#"
+
+mat = [[1,2,3],[4,5,6],[7,8,9]]
+shift_col(mat, 0, 1)
+print(mat)
+shift_row(mat, 0, 1)
+print(mat)
+rect(mat, 1, 1)
+print(mat)
 
 
 def handle(ln):
@@ -24,14 +42,10 @@ def handle(ln):
         ln = ln.split(" ")
         print("column", ln[-3][2:], ln[-1])
 
+file = open("input.txt")
+for line in file:
+    print(line[:-1])
+    handle(line)
+    # print(line)
 
-
-#
-# file = open("input.txt")
-# for line in file:
-#     print(line[:-1])
-#     handle(line)
-#     # print(line)
-
-print(shift_row([1,2,3,4,5], 5))
 
