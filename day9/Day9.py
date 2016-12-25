48b9f7b8157183346a4299ab881bb313dc220b90
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-12-25 22:32:25 +0000

    day 9 forgot to stipr white space

--- day9/Day9.py
+++ day9/Day9.py
@@ -2,22 +2,25 @@ import re
 
 
 def decompress(some_string, num_chars, num_reps):
+    # multiply selected section and add to start of string passed to function
     return (some_string[:num_chars] * num_reps) + some_string[num_chars:]
 
 
 def parse(some_string, p=True):
     i = 0
     while i < len(some_string):
-        print(i)
+        print("i=", i, some_string[i:i+10])
         if some_string[i] == "(":
             # from i onwards get instruction (a X b), remove "(", split on x
             instruction = some_string[i:].split(")")[0][1:]
 
+            if p:
+                print("trim", some_string[i: i+len(instruction)+2])
+
+            print("pre", some_string[i:i+10])
             # remove instruction from string
             some_string = some_string[:i] + some_string[i+len(instruction)+2:]
-
-            if p:
-                print("trim", some_string)
+            print("post", some_string[i:i+10])
 
             # create list of integers: convert ro integer (remove "(", slit on x
             instruction = list(map(lambda x: int(x), instruction.split("x")))
@@ -30,7 +33,7 @@ def parse(some_string, p=True):
             i += (instruction[0] * instruction[1])
 
             if p:
-                print(instruction)
+                print("instruction", instruction, "i=", i)
                 print(some_string)
 
             # skip over increment
@@ -40,7 +43,7 @@ def parse(some_string, p=True):
     return some_string
 
 
-# # tests
+# tests
 # one = parse("A(1x5)BC", False)
 # print(len(one))
 # two = parse("(3x3)XYZ", False)
@@ -49,14 +52,18 @@ def parse(some_string, p=True):
 # print(len(three))
 # four = parse("(6x1)(1x3)A", False)
 # print(len(four))
-# five = parse("X(8x2)(3x3)ABCY", False)
-# print(len(five))
+# five = parse("X(8x2)(3x3)ABCY(2x11)abcd", False)
+# print(five)
 
 parse("x(2x2)ab(1x4)asdf")
-#
-# file = open("input.txt")
-# for line in file:
-#     print("here", len(parse(line)))
 
+file = open("input.txt")
+for line in file:
+    ans = parse(line, False)
+    print("here", len(ans.strip()))
+
+# parse("ORNXNQJQ(151x7)(5x9)OFIXU(27x3)(21x9)VDCYQELDJQUAFZUHFZVSU(34x15)(12x10)SEDIUUVFPEKY(3x9)NHR(1x11)I(15x6)(9x13)"
+#       "CMNDUYGYR(40x6)(4x7)RMNG(25x8)XPDSEYNCWFQFAKUMITWMBLMIK(7x11)(1x10)N(109x1)(101x15)")
 # 128276 too high
 # 123909 too high
+# 123908 -_- forgot to strip white space
