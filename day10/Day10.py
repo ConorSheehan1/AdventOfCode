2d83238e07c1b95809752321da2637f504e2788f
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-12-26 12:38:56 +0000

    checkpoint before changin approach, initialise all bots necessary at start

--- day10/Day10.py
+++ day10/Day10.py
@@ -1,13 +1,19 @@
 class Bot:
-    def __init__(self, num, value):
+    def __init__(self, num, value=None, instruction=""):
         self.instruction = ""
-        self.values = [value]
         self.num = num
+        self.values = [value]
 
     def print(self):
         print("bot number:", self.num, self.values, self.instruction)
 
+    def exectue(self):
+        low = self.instruction[self.instruction.find("low"):self.instruction.find("and")]
+        high = self.instruction[self.instruction.find("high"):]
+        print(low, high)
+
 bots = []
+output = {}
 file = open("test.txt")
 for line in file:
     if line.startswith("value"):
@@ -23,12 +29,21 @@ for line in file:
         else:
             print("new bot")
             bots.append(Bot(int(line[-1]), int(line[1])))
+        # skip back to top of loop
+        continue
 
-        # print bots in list
+    if line.startswith("bot"):
+        line = line.split(" ")
         for bot in bots:
-            bot.print()
-        print()
+            if bot.num == int(line[1]):
+                print("old bot")
+                bot.instruction = " ".join(line[2:])
+                break
+        else:
+            print("new bot")
+            bots.append(Bot(int(line[1]), int(line[1])))
+        continue
 
-
-
-    # " ".join(line[:-1])
+# print bots in list
+for bot in bots:
+    bot.print()
